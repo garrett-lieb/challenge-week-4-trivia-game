4da53f4f42c87d5859b12aa7b4606512afc4d1c0
garrett lieb <liebg08@gmail.com>
2023-11-21 15:22:32 -0800

    added questions and answers to draft sheet

--- draft-script.js
+++ draft-script.js
@@ -1,43 +1,43 @@
 [
-    "question: Commonly used data types DO NOT include:",
-    "answer1: "
-    "answer2: "
-    "answer3: "
-    "answer4: "
+    "question1: Commonly used data types DO NOT include:",
+    "answer1-1: boolean"
+    "answer2-1: number"
+    "answer3-1: algorithm"
+    "answer4-1: string"
 ]
 
 
 [
-    "question: The condition in an if / else statement is enclosed within ____.",
-    "answer1: "
-    "answer2: "
-    "answer3: "
-    "answer4: "
+    "question2: The condition in an if / else statement is enclosed within ____.",
+    "answer1-2: ' ' "
+    "answer2-2: { } "
+    "answer3-2: [ ] "
+    "answer4-2: ( )"
 ]
 
 
 [
     "question: Arrays in JavaScript can be used to store ____.",
-    "answer1: "
-    "answer2: "
-    "answer3: "
-    "answer4: "
+    "answer1: a list of values"
+    "answer2: a list of variables"
+    "answer3: a list of strings"
+    "answer4: a list of groceries"
 ]
 
 
 [
     "question: String values must be enclosed within ____ when being assigned to variables.",
-    "answer1: "
-    "answer2: "
-    "answer3: "
-    "answer4: "
+    "answer1: double quotes"
+    "answer2: single or double quotes"
+    "answer3: single quotes"
+    "answer4: brackets"
 ]
 
 
 [
-    "question: A very useful tool used during development and debugging for printing content to the debugger is:",
-    "answer1: "
-    "answer2: "
-    "answer3: "
-    "answer4: "
+    "question: Contorl flow is the order in which ____ are executed or evaluated when a program is run.",
+    "answer1: alogrithms"
+    "answer2: boolean values"
+    "answer3: arrays, variables, and strings"
+    "answer4: individual statements, instructions or function calls"
 ]
