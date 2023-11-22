b3caf715f9bb7cb8823783e7af7ab68444c82e04
garrett lieb <liebg08@gmail.com>
2023-11-21 21:35:59 -0800

    add css and js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,32 +6,40 @@ var answer1 = document.querySelector("#answer1");
 var answer2 = document.querySelector("#answer2");
 var answer3 = document.querySelector("#answer3");
 var answer4 = document.querySelector("#answer4");
-
+var rootEl = $("#answer-feedback");
 
 var correct = document.querySelector("#correct");
 var incorrect = document.querySelector("#incorrect");
-var answerfeedback = document.querySelector("#answer-feedback");
 
 var scores = document.querySelector("#scores");
 
 var chosenAnswer = "";
-var highscores = "";
+var Highscores = "";
 var isWin = false;
 var timer;
 var timerCount;
 
+
+// array of questions
+
+
+// array of answers
 
 
 startButton.addEventListener("click", startGame);
 
 function startGame() {
     isWin = false;
+    isLoss = false;
     timerCount = 5;
     // Prevents start button from being clicked when round is in progress
     startButton.disabled = true;
+    // load questions
+    // load answers
     startTimer()
   }
 
+  
 // create timer function
 function startTimer() {
     // Sets timer
@@ -46,8 +54,6 @@ function startTimer() {
         if (isWin && timerCount > 0) {
           // Clears interval and stops timer
           clearInterval(timer);
-          winGame();
-          getInitials();
         }
       }
       // Tests if time has run out
@@ -55,48 +61,15 @@ function startTimer() {
         timerElement.textContent = "You ran out of time :(";
         // Clears interval
         clearInterval(timer);
-        loseGame();
-        getInitials();
       }
     }, 1000); 
   }
 
-// if end game display score and prompt to enter initials
-// if initials are entered display highscores with local storage
 
-function winGame() {
-}
-
-function loseGame() {
-}
-
-function getInitials() {
-    prompt("Enter your initials");
-}
-  
+// check win
 
 
-
-
-// create array of questions 
-// create array of answers
-// create array of correct answers
-// create array of incorrect answers
-// Creates blanks on screen
-function renderBlanks() {
-    // Randomly picks word from words array
-    chosenWord = words[Math.floor(Math.random() * words.length)];
-    lettersInChosenWord = chosenWord.split("");
-    numBlanks = lettersInChosenWord.length;
-    blanksLetters = []
-    // Uses loop to push blanks to blankLetters array
-    for (var i = 0; i < numBlanks; i++) {
-      blanksLetters.push("_");
-    }
-    // Converts blankLetters array into a string and renders it on the screen
-    wordBlank.textContent = blanksLetters.join(" ")
-  }
-
+// check loss
 
 
 // if answer if correct display correct
--- draft-script.js
+++ draft-script.js
@@ -1,3 +1,8 @@
+
+// array of questions
+//array of answers?
+
+
 [
     "question1: Commonly used data types DO NOT include:",
     "answer1-1: boolean"
@@ -41,3 +46,57 @@
     "answer3: arrays, variables, and strings"
     "answer4: individual statements, instructions or function calls"
 ]
+
+// if end game display score and prompt to enter initials
+// if initials are entered display highscores with local storage
+// if answer is correct add 10 points to score
+// if answer is incorrect subtract 10 seconds from timer
+
+//if all questions are answered &&& time !== 0 end game
+// if time === 0 end game
+// if game ends prompt to enter initials and save score to local storage
+// if initials are entered display highscores with local storage
+
+function winGame() {
+}
+
+function loseGame() {
+
+}
+
+function getInitials() {
+    var initials {
+        prompt("Enter your initials");
+        
+    }
+}
+
+function saveHighscore() {
+    var Highscores = {
+        student: student.value,
+        grade: grade.value,
+        comment: comment.value.trim(),
+      };
+      // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
+      localStorage.setItem('studentGrade', JSON.stringify(studentGrade));
+    }
+}
+
+// create array of questions 
+// create array of answers
+// create array of correct answers
+// create array of incorrect answers
+// Creates blanks on screen
+function renderBlanks() {
+    // Randomly picks word from words array
+    chosenWord = words[Math.floor(Math.random() * words.length)];
+    lettersInChosenWord = chosenWord.split("");
+    numBlanks = lettersInChosenWord.length;
+    blanksLetters = []
+    // Uses loop to push blanks to blankLetters array
+    for (var i = 0; i < numBlanks; i++) {
+      blanksLetters.push("_");
+    }
+    // Converts blankLetters array into a string and renders it on the screen
+    wordBlank.textContent = blanksLetters.join(" ")
+  }
